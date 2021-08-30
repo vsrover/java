d1f231fa5b094a58d3b809469193abf7d49d75e7
vsrover <opttps@mail.ru>
2021-08-30 10:26:04 +0300

    log file rolling

--- src/main/java/com/vah/august/log/Main.java
+++ src/main/java/com/vah/august/log/Main.java
@@ -3,27 +3,31 @@ package com.vah.august.log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 public class Main {
-    static int count = 0;
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger2 = LoggerFactory.getLogger("analytics");
 
     public static void main(String[] args) {
         logger.error("omg {}", new Object());
+        logger2.error(" fuck {}", new Analitic(1, "Fuck"));
+    }
+}
 
-        for (int i = 0; i < 4; i++) {
-            MDC.put("logFileName", getName());
-            logger.error("hello");
-            MDC.remove("logFileName");
-        }
+class Analitic {
+    int i;
+    String s;
 
+    public Analitic(int i, String s) {
+        this.i = i;
+        this.s = s;
     }
 
-    private static String getName() {
-        count++;
-        return "omg " + count;
+    @Override
+    public String toString() {
+        return "Analitic{" +
+                "i=" + i +
+                ", s='" + s + '\'' +
+                '}';
     }
-
-
 }
