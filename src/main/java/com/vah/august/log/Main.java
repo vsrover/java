feaad29e83e9e68a222f29570a0dcb2ad773b054
vsrover <opttps@mail.ru>
2021-08-30 10:48:50 +0300

    log file rolling

--- src/main/java/com/vah/august/log/Main.java
+++ src/main/java/com/vah/august/log/Main.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static final Logger logger2 = LoggerFactory.getLogger("integration-transaction-err");
+    private static final Logger logger2 = LoggerFactory.getLogger("integration-transaction-fail");
 
     public static void main(String[] args) {
         logger.error("omg {}", new Object());
